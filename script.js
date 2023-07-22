ce2bbcad8e8d2b05628af233d75dbd45bd07aac3
Nazmul-bari <92978372+Nazmul-bari@users.noreply.github.com>
2023-07-22 23:11:06 +0600

    Add files via upload
    I have added html css js file

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlaider(anything){
+document.querySelector('.starbucks').src=anything;
+}
+function changeColor(color){
+var circle= document.querySelector('.circle')
+circle.style.bacground=color;
+}
+
